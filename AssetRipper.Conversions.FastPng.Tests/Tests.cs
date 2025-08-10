c2e243f88bac0798d153669e2c2f7a7eae0e71c3
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-08-09 19:57:51 -0700

    Add gradient unit test

--- AssetRipper.Conversions.FastPng.Tests/Tests.cs
+++ AssetRipper.Conversions.FastPng.Tests/Tests.cs
@@ -7,4 +7,35 @@ public class Tests
 	{
 		Assert.Throws<ArgumentOutOfRangeException>(() => FPng.EncodeImageToMemory([], 0, 0));
 	}
+
+	[Test]
+	public void EncodingGradientIsSuccessful()
+	{
+		int width = 1024;
+		int height = 1024;
+		byte[] rawPixelData = CreatePixels(width, height);
+		byte[] encodedData = FPng.EncodeImageToMemory(rawPixelData, width, height);
+		Assert.That(encodedData, Is.Not.Empty);
+	}
+
+	static byte[] CreatePixels(int width, int height)
+	{
+		int index = 0;
+		byte[] data = new byte[width * height * 3]; // RGB format
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				// Create a simple gradient
+				int r = (x * 255) / width;
+				int g = (y * 255) / height;
+				int b = ((x + y) * 255) / (width + height);
+
+				data[index++] = (byte)r;
+				data[index++] = (byte)g;
+				data[index++] = (byte)b;
+			}
+		}
+		return data;
+	}
 }
